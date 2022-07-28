0a981885c1d48471be45fad234e87af0fdda8715
Sujit Singh <sujit510@gmail.com>
2022-07-28 13:34:13 +0530

    Added base code for react and webpack

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// import reactDom from "react-dom";
+import App from './src/App'
+import ReactDOM from 'react-dom/client'
+
+// reactDom.render(<App />, document.getElementById("root"));
+// reactDom.cre
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>
+      Welcome to React App thats build using Webpack and Babell separately!!
+    </h1>
+  )
+}
+
+export default App
